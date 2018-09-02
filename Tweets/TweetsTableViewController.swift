d50dc470cdee7a1ecbd12c8b3bb97e63c6d55a3e
Ilya Semerukhin <semerukhin@gmail.com>
2018-09-02 16:22:29 +0300

    Set up a new project

---
+++ Tweets/TweetsTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TweetsTableViewController.swift
+//  Tweets
+//
+//  Created by Ilya Semerukhin on 02.09.2018.
+//
+
+import UIKit
+
+class TweetsTableViewController: UITableViewController {
+   
+   // MARK: - UITableViewDataSource
+   
+   override func numberOfSections(in tableView: UITableView) -> Int {
+      // #warning Incomplete implementation, return the number of sections
+      return 0
+   }
+   
+   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      // #warning Incomplete implementation, return the number of rows
+      return 0
+   }
+   
+   /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Tweet", for: indexPath)
+    
+    // Configure the cell...
+    
+    return cell
+    }
+    */
+   
+}
+
+
+
+
+
+
